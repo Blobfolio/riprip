b3a89e7f14ca5c9ffa43112e9c3eaaf0e8a59729
elmattic <elmattic@users.noreply.github.com>
2026-07-10 12:13:42 +0200

    Fix build

--- riprip_core/src/usb/bot.rs
+++ riprip_core/src/usb/bot.rs
@@ -68,6 +68,6 @@ impl CommandStatusWrapper {
     }
 
     pub(super) fn is_valid(&self, tag: u32) -> bool {
-        csw.signature == CSW_SIGNATURE && csw.tag == tag 
+        self.signature == CSW_SIGNATURE && self.tag == tag 
     }
 }
