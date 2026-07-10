a64f059f6bb3387d7186325f377a613d1150a176
elmattic <elmattic@users.noreply.github.com>
2026-07-10 17:39:16 +0200

    Add linux impl

--- riprip_core/src/usb/mod.rs
+++ riprip_core/src/usb/mod.rs
@@ -8,9 +8,8 @@ Somewhat useful documentation:
 
 mod bot;
 mod cdtext;
+mod device;
 mod language;
-#[cfg(target_os = "macos")]
-mod macos;
 mod mmc;
 
 use crate::{
@@ -180,7 +179,7 @@ impl<C: UsbContext> LibusbInstance<C> {
             .map_err(|e| RipRipError::DeviceOpen(Some(e.to_string())))?;
 
         let device_handle = if let Some(path) = dev {
-            if let Some((vid, pid)) = macos::get_device_desc(&path)? {
+            if let Some((vid, pid)) = device::get_desc(&path)? {
                 // eprintln!("{vid:04x}:{pid:04x}");
                 find_and_open_device(devices, vid, pid)?
             } else {
